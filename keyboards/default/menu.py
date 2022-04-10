18355f192ad7a708dc448a1bfcf1d789b0de8447
Ako04 <akimzhanbaltabay@gmail.com>
2022-04-10 14:24:37 +0600

    Aiogram buttons Akimzhan

---
+++ keyboards/default/menu.py
@@ -0,0 +1,14 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="2E"),
+        ],
+        [
+            KeyboardButton(text="4E"),
+            KeyboardButton(text="3I")
+        ],
+    ],
+    resize_keyboard=True
+)
